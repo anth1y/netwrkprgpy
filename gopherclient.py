50911591cfb6e896c84b4cc03a219d0567fe4e56
ant <anth1y@gmail.com>
2016-06-12 20:57:39 -0700

    Adding some error handling

--- gopherclient.py
+++ gopherclient.py
@@ -8,7 +8,11 @@ host = sys.argv[1]
 filename = sys.argv[2]
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((host, port))
+try:
+    s.connect((host, port))
+except socket.gaierror, e:
+    print "Error connecting to server: %s" % e
+    sys.exit(1)
 s.sendall(filename + "\r\n")
 
 while 1:
